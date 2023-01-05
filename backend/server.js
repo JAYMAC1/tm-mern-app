f00b36e96fb7676b672cc575b7eddf675ad9cd8c
James McNeil <mcneil_james@hotmail.com>
2023-01-05 22:24:15 +0000

    added env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('server..')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
