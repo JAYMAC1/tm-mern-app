d767f5b8578ef06ee41086a98ac52d167fd65ccb
James McNeil <mcneil_james@hotmail.com>
2023-01-09 23:01:45 +0000

    impemented authMiddleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -77,7 +77,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.send('me')
+  // res.status(200).json(req.user.id)
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
   // const user = { id: req.user._id, name: req.user.name, email: req.user.email }
   // res.status(200).json(user)
 })
