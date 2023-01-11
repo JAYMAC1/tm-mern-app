a490df04c2cd5a75cfb7895c7d940426e0eba064
James McNeil <mcneil_james@hotmail.com>
2023-01-11 22:38:13 +0000

    created home login and rgister pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
-  return <div>my app</div>
+  return (
+    <>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
