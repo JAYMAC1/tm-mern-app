4aac4859ffdfbedf30c0b60601a69456c4a56d92
James McNeil <mcneil_james@hotmail.com>
2023-01-11 22:44:21 +0000

    creatign Header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <div className='container'>
           <Routes>
-            <Route path='/' element={<Home />} exact />
+            <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {FaSignInAlt. FaSignoutAlt, FaUser} from 'react-icons'
+
+function Header() {
+  return (
+    <div>Header
+
+    </div>
+  )
+}
+
+export default Header
